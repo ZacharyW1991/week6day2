d0cc734c37b3c26145ecd9e15573a13cab888c29
ZacharyW1991 <ZacharyW1991@outlook.com>
2023-10-24 21:09:06 -0400

    First

---
+++ phonebook/routes.py
@@ -0,0 +1,19 @@
+from phonebook import app
+from flask import render_template, url_for, redirect
+from phonebook.forms import SubmitForm
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit')
+def submission():
+    form=SubmitForm()
+    if form.validate_on_submit():
+        first_name=form.first_name.data
+        last_name=form.last_name.data
+        phone=form.phone.data
+        address=form.address.data
+        print(first_name, last_name, address, phone)
+        return redirect(url_for('index'))
+    return render_template('submit.html', form=form)
